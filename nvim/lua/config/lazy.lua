c1c24d47a7570bdbf33e1e705e80a3188b7e935c
Zulfikar Eka Sagata <zulfikar@ruangguru.com>
2024-10-11 20:36:45 +0700

    add urst

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.dap.core" },
     -- import/override with your plugins
     { import = "plugins" },
